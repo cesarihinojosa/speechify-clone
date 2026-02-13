414ba9dc082428bf07874f61981dc30553d74592
Cesar Hinojosa <cesarivanhinojosa@gmail.com>
2026-02-13 10:02:21 -0600

    Initial Commit

---
+++ SpeechifyClone/SpeechifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechifyCloneApp.swift
+//  SpeechifyClone
+//
+//  Created by Cesar Hinojosa on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct SpeechifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
